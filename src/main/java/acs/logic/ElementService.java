fe263498b836aa8ddc4b16884a5b234dd3256b43
Amir <Amir@DESKTOP-0O4L3I4>
2020-04-18 13:28:21 +0300

    create userEntity

---
+++ src/main/java/acs/logic/ElementService.java
@@ -0,0 +1,14 @@
+package acs.logic;
+
+import java.util.List;
+
+import acs.boundaries.ElementBoundary;
+
+public interface ElementService {
+public ElementBoundary create(String managerEmail,ElementBoundary element);
+public ElementBoundary update(String mangerEmail,String elementid,ElementBoundary update);
+public List<ElementBoundary> getAll(String userEmail);
+public ElementBoundary getSpecificElement(String userEmail,String elementId);
+public void deleteAllElements(String adminEmail);
+
+}
