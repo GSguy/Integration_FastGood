7051d28504cef187706c0a30e1dd3cf700125212
oshtoy <oshri1992@gmail.com>
2020-05-28 10:54:33 +0300

    add PgSQL

--- src/main/java/acs/logic/ActionServicesWithDB.java
+++ src/main/java/acs/logic/ActionServicesWithDB.java
@@ -49,26 +49,30 @@ public class ActionServicesWithDB implements ActionServiceUpgraded {
 	@Override
 	@Transactional //(readOnly = false)
 	public Object invokeAction(ActionBoundary action) {
-		String elementId=action.getElement().get("elementId");
-		String email=action.getInvokedBy().get("email");
-		if(elementId==null|| email==null) {
+		String elementId = action.getElement().get("elementId");
+		String email = action.getInvokedBy().get("email");
+		if(elementId == null|| email == null) {
 			throw new RuntimeException("cannot invoke action without element id or inovkedby");
 		}
 		
 		UserEntity user=this.userDao.findOneByEmail(email);
-		if(user==null) {
-			throw new  EntityNotFoundException ("could not find any  user with  email : " + email);
+		
+		if(user == null) {
+			throw new  EntityNotFoundException ("could not find any user with  email : " + email);
 		}
 		if(user.getRole()!=UserRole.PLAYER) {
 			throw new RuntimeException("only users with player role can invoke actions");
 		}
+		
 		ElementEntity element=this.elementDao.findOneByElementIdAndActive(Long.parseLong(elementId), true);
 		if(element==null) {
 			throw new EntityNotFoundException("could not find active Element for id : " + elementId);
 		}
+		
 		// create new tupple in the idValue table with a non-used id
 		LastIdValue idValue = this.lastValueDao.save(new LastIdValue());
 		ActionEntity entity = this.actionEntityConverter.toEntity(action);
+		
 		// set Server fields
 		entity.setActionID(idValue.getLastId()); //create new ID - Not consider user ID INPUT
 		this.lastValueDao.delete(idValue);// cleanup redundant data
